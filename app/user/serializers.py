585a8e30e40837e7ed03526508b6166dbc21d883
Amisha Agrawal <mishi4a3@gmail.com>
2024-09-01 15:51:28 -0400

    Add user token authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,12 @@
 """
 Serializers for the user API view
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+    )
+from django.utils.translation import gettext as _
+
 
 from rest_framework import serializers
 
@@ -19,3 +24,43 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        # instance: model instance that is being updated
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+# defining a seriealizer with 2 fields. Input type password makes
+# it hidden when using browsable API
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attribs):
+        """Validate and authenticate the user."""
+        email = attribs.get('email')
+        password = attribs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attribs['user'] = user
+        return attribs
